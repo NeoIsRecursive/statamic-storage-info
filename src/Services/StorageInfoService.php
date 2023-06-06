0910e2df7dab4c7b98ed42a3d8f0e18db3136c5a
NeoIsRecursive <neo.leijondahl@gmail.com>
2023-06-06 17:50:32 +0200

    Init unused

--- src/Services/StorageInfoService.php
+++ src/Services/StorageInfoService.php
@@ -3,6 +3,8 @@
 namespace Neoisrecursive\StorageInfo\Services;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\File;
+use Statamic\Assets\AssetCollection;
 use Statamic\Assets\AssetContainer as Container;
 use Statamic\Facades\AssetContainer;
 use Statamic\Support\Str;
@@ -19,19 +21,42 @@ class StorageInfoService
         return Cache::forget('neoisrecursive:storage-info');
     }
 
-    public static function getContainerData(array $containers)
+    public function getContainerData(array $containers)
     {
-        $containers = AssetContainer::all()->filter(function ($container) use ($containers) {
-            return in_array($container->handle(), $containers);
-        })->map(function (Container $container) {
+        $containers = $this->getAssets($containers)->map(function (Container $container) {
+            $assets = $container->assets();
             return [
                 'name' => $container->title(),
                 'files' => $container->queryAssets()->count(),
                 'url' => $container->showUrl(),
-                'spaceUsed' => Str::fileSizeForHumans($container->assets()->sum(fn ($asset) => $asset->size())),
+                'spaceUsed' => Str::fileSizeForHumans($assets->sum(fn ($asset) => $asset->size())),
+                'unused' => $this->getUnused($assets)->count(),
             ];
         });
 
         return $containers;
+    }
+
+    public function getAssets(array $containers){
+        return AssetContainer::all()->filter(function ($container) use ($containers) {
+            return in_array($container->handle(), $containers);
+        });
+    }
+
+    public function getUnused(AssetCollection $assets){
+        return collect('content')
+            ->map(fn ($folder) => File::allFiles(base_path($folder)))
+            ->flatten()
+            ->unique()
+            ->each(function ($contentFile) use ($assets) {
+                $contents = file_get_contents($contentFile);
+
+                $assets->each(function ($asset, $index) use ($contents, $assets) {
+                    // If asset is used in content, then remove it from unused list.
+                    if (strpos($contents, $asset->path()) !== false) {
+                        $assets->forget($index);
+                    }
+                });
+            });
     }
 }
